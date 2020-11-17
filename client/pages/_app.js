66e0ad7dbbbfa76337f9305a28d09908ca35f130
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-11-16 21:45:00 -0300

    Create server with  socket io and initialize it on the client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import socketIOClient from 'socket.io-client';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from 'stylesUtils/globalStyles';
@@ -7,12 +8,21 @@ import theme from 'stylesUtils/theme';
 
 import GameContainer from 'src/components/GameContainer/GameContainer';
 
+const SERVER = 'http://localhost:8080';
+
 function App({ Component, pageProps }) {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const socket = socketIOClient(SERVER);
+    setSocket(socket);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <GameContainer>
-        <Component {...pageProps} />
+        <Component {...pageProps} socket={socket} />
       </GameContainer>
     </ThemeProvider>
   );
---
+++ server/src/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = socketIo(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  }
+});
+
+httpServer.listen(8080, () => {
+  console.log('listening on *:8080');
+});
+
+let player1Socket = null
+let player2Socket = null
+
+io.on('connection', (socket) => {
+  console.log('connected')
+  let player
+
+  if (player1Socket === null) {
+    player1Socket = socket;
+    player =  'player1'
+  } else if (player2Socket === null) {
+    player2Socket = socket;
+    player =  'player2'
+  }
+
+  socket.emit('player', player);
+
+  socket.on('number to start', (number)   =>  {
+    console.log('number to start', {number})
+    const nextPlayer = socket === player1Socket ? 'player2' : 'player1';
+    io.emit('number to start', {number, nextPlayer});
+  })
+
+  socket.on('player move', ({addition, number}) => {
+    console.log('player move', {addition, number})
+    const nextPlayer = socket === player1Socket ? 'player2' : 'player1';
+    io.emit('player move', {addition, number, player, nextPlayer});
+  } )
+
+  socket.on('disconnect', () => {
+    console.log('disconnected', {player})
+    if (player === 'player1') {
+      player1Socket = null;
+    } else if (player === 'player2') {
+      player2Socket = null;
+    }
+  })
+});
+
+
+
+
+
