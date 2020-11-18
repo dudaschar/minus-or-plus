0a825ee131e81f31b165f8e326d94fd5b8bba977
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-11-17 22:50:30 -0300

    Resolve responsivity to desktop

--- client/src/components/GameContainer/styles/Container.js
+++ client/src/components/GameContainer/styles/Container.js
@@ -4,15 +4,19 @@ export const Container = styled.div`
   min-height: 100vh;
   background-color: ${({ theme }) => theme.colors.primaryLight};
   display: flex;
-  flex-direction: column;
   position: relative;
+  justify-content: center;
 `;
 
 Container.displayName = 'Container';
 
 export const ContentWrapper = styled.div`
   padding: ${({ theme }) => theme.spacing.medium};
-  display: flex;
   flex: 1;
+  display: block;
+  flex-shrink: 0;
   margin-top: 5.75rem;
+  max-width: 470px;
 `;
+
+ContentWrapper.displayName = 'ContentWrapper';
